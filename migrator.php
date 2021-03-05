4595b8ebe1219ae1689626572abea31ed2f4eb51
jday <jday@teedex.net>
2021-03-05 02:39:29 +0200

    Added db scan and showing tables and columns

--- migrator.php
+++ migrator.php
@@ -17,10 +17,51 @@ if( !is_dir($migration_folder) ) {
     echo "\e[33mWARNING\e[0m: The migration directory does not exists, crate ? Y/N\r\n";
     $result = readline();
     if(!preg_match('~[y]~ui', $result)) {
-	die("Bye!");
+	    die("Bye!");
     } else {
-	mkdir($migration_folder);
+	    mkdir($migration_folder);
     }
 }
 
 // todo: main code here
+
+$config_file = $config_folder . DIRECTORY_SEPARATOR . 'main.php';
+
+if( !file_exists($config_file) ) {
+    die("\e[31mFATAL ERROR\e[0m: The configuration file main.php does not exists");
+}
+try {
+    
+    $yii = $current_dir . '/../../framework/yii.php';
+    require_once($yii);    
+    
+    $config = require_once($config_file);    
+    unset($config['defaultController']);
+    unset($config['components']['log']);    
+    $app=Yii::createApplication('CConsoleApplication', $config);
+    $db_connection = [];
+
+    foreach($config['components'] as $name => $params) {
+        if (isset($params['class']) && preg_match('~\.db\.~ui', $params['class'])) {
+            $db_connection[$name] = $params;
+        }
+    }
+
+    foreach($db_connection as $db_name => $db_params) {
+        echo "\e[33mDB\e[0m: $db_name".PHP_EOL;        
+        $tables = Yii::app()->$db_name->schema->getTables();
+        echo "\e[32mTABLES\e[0m:".PHP_EOL;
+        foreach($tables as $table) {
+            echo " - \e[36m$table->name\e[0m".PHP_EOL;
+            foreach($table->columns as $column=>$shema) {
+                echo "     | ".($table->primaryKey == $column?"\e[44m$column\e[0m":"$column").PHP_EOL;
+            }
+        }
+        
+    }
+
+} catch (Throwable $t) {
+    die("\e[31mFATAL ERROR\e[0m: " . $t->getMessage() . "\r\n");
+} catch (Exception $e) {
+    die("\e[31mFATAL ERROR\e[0m: ".$e->getMessage() . "\r\n");
+}
